0c3bc523d069e5d91e53c2f76c41d14217414352
Mariano Villarreal <marianov994@gmail.com>
2021-08-21 17:13:35 -0300

    remove useless js

--- src/js/lottie.js
+++
@@ -1,9 +0,0 @@
-import lottie from 'lottie-web';
-
-let animation = lottie.loadAnimation({
-  container: document.getElementById('bm'),
-  renderer: 'svg',
-  loop: true,
-  autoplay: true,
-  path: './assets/json/lottie-data.json'
-});
--- src/js/vanilla.js
+++ src/js/vanilla.js
@@ -1,22 +1,15 @@
-console.log('VANILLA MODULE');
-
-// Selectors
+// selectors (returns dom element)
 const $element_general_selector_search = document.querySelector('.main #panel_one');
 const $element_general_selector = document.querySelector('.autor__container');
 const $element_id_selector = document.getElementById('panel_one');
 const $element_class_selector = document.getElementsByClassName('date__container');
-// console.log({
-//   $element_general_selector_search,
-//   $element_class_selector,
-//   $element_general_selector,
-//   $element_id_selector
-// });
 
-// Templates literal
+// template strings
 
 // create tools
 const $element_id = document.getElementById('panel_one');
 const $html = document.implementation.createHTMLDocument();
+
 const ItemTemplate = (src, title) =>
   `<div class="primaryPlaylistItem">
         <div class="primaryPlaylistItem-image">
@@ -26,45 +19,21 @@ const ItemTemplate = (src, title) =>
           ${title}
         </h4>
       </div>`;
+
 // generate template
-const image_url =
-  'https://www.google.com/logos/doodles/2019/200th-anniversary-of-museo-del-prado-4899096010686464.3-l.png';
-$HTMLStringElement = ItemTemplate(image_url, 'Google');
-// append template
+$HTMLStringElement = ItemTemplate(
+  'https://www.google.com/logos/doodles/2019/200th-anniversary-of-museo-del-prado-4899096010686464.3-l.png',
+  'Google'
+);
 $html.body.innerHTML = $HTMLStringElement;
 $element_id.append($html.body.children[0]);
 
-// Styles
-// $element_id.classList; // to see array class
-// $element_id.classList.add('active');
-// $element_id.classList.remove('active');
-// $element_id.classList.toggle('active'); // add or remove
-// $element_id.style.border = '10px solid red';
-
-// SIMULATING components (2 examples)
-
-const text = () => {
-  document.body.innerHTML = '<p>mariano</p>';
-};
-
-// function render(data) {
-//   const $element = document.createElement('img');
-//   $element.setAttribute('src', data.imageUrl);
-//   // now must show it in the browser display (to end body's)
-//   document.body.append($element);
-// }
-// // * some fetch
-// render(data);
-
-// CREATE elements and attributes assignation
-
-function setAtributes($element, attributes) {
-  for (const attribute in attributes) {
-    // assign to $element each attribute in the attributes object
-    // we can use getAttribute to comprobate
-    $element.setAtribute(attribute, attributes[attribute]);
-  }
-}
+// handle styles
+$element_id.classList; // to see array class
+$element_id.classList.add('active');
+$element_id.classList.remove('active');
+$element_id.classList.toggle('active'); // add or remove
+$element_id.style.border = '10px solid red';
 
 // add a loading element when the user submit
 $form.addEventListener('submit', event => {
